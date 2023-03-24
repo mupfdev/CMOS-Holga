c6a36c32785645b418cdbe43c647afe6dbc18d53
Michael Fitzmayer <mail@michael-fitzmayer.de>
2023-03-24 13:58:10 +0100

    Upscale images when converting from PGM to JPG

--- sdcard/convert.bat
+++ sdcard/convert.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 for /r %%f in (RAW\*.PGM) do (
   if not exist DCIM_%%~nf.JPG (
-    convert %%f DCIM_%%~nf.JPG
+    convert -scale 512x504 %%f DCIM_%%~nf.JPG
   )
 )
 endlocal
